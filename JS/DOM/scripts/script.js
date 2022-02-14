0871d22d8f5a727f462d1508484ba884b0063793
Seena2 <dinku22@gmail.com>
2022-02-14 09:26:24 +0300

    cover new lessons

--- JS/DOM/scripts/script.js
+++ JS/DOM/scripts/script.js
@@ -75,4 +75,123 @@ console.log(Node.ELEMENT_NODE); //logs 1,one is anumeric code value for element 
       */
      //SELECTORS -to query the DOM
      //GETELEMENT BY ID
-     console.log(document.getElementById('header-tile'));
+     var headerTitle=document.getElementById('header-title');
+     var headerMenu=document.getElementById('main-header');
+     console.log(header);
+     /*
+     console.log(headerTitle);
+     headerTitle.textContent='changes the h1 text content';
+     headerTitle.innerText="this also changes the h1 content";//pays atension to element styling
+    headerTitle.innerHTML="<h3>this also change content of h1<h3>";//puts this elment inside the target elmt
+    */
+   //CHANGE STYLES
+   headerMenu.style.borderBottom='solid 3px black';//NB u cant use border-bottom we use camelCase instead
+    //GETELEMENT BY CLASS NAME :
+    /*
+    var items=document.getElementsByClassName('list-group-item');
+    console.log(items);//returns collection
+    console.log(items[1]); // to access one item
+    items[1].textContent ="Hello";
+    items[1].style.fontWeight='bold';
+    items[1].style.backgroundColor='yellow';
+    //items.style.backgroundColor='#f4f4f4'; //gives error-do Not work, b/c its a collection and we need to loop to add style
+    for(let i=0;i<items.length;i++){
+        items[i].style.backgroundColor='#f4f4f4';
+    }
+*/
+    //GETELEMENT BY TAG NAME
+    /*
+    var li=document.getElementsByTagName('li');//grabs all the li elements
+    console.log(li);
+    console.log(li[1]); // to access one item
+    li[1].textContent ="Hello";
+    li[1].style.fontWeight='bold';
+    li[1].style.backgroundColor='yellow';
+    for(let i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4';
+    }
+    */
+   //QUERY SELECTOR: works l ike jquery but only grabs the first item it finds
+   /*
+   let headr=document.querySelector('#main-header');
+   headr.style.borderRight='solid 4px grey';
+   let input= document.querySelector('input');
+   input.value="Hello world";
+   let submit=document.querySelector('input[type="submit"]');
+   submit.value='Add Item';
+   let itm=document.querySelector('.list-group-item');
+   itm.style.color='red'; //affects only the first item it finds
+   let lastitem=document.querySelector('.list-group-item:last-Child');
+   lastitem.style.color='blue';
+   let secondItem=document.querySelector('.list-group-item:nth-child(2)');
+   secondItem.style.color='coral';
+    */
+   //QUERYSELECTOR ALL// : grabs all elements specified
+   /*
+   let titles=document.querySelectorAll('.title');
+   console.log(titles);
+   titles[0].textContent="Eehhehhe";
+    //alternet items selection
+    let odd=document.querySelectorAll('li:nth-child(odd');
+    for(let w in odd){
+        odd[w].style.background='#f4f4f4';
+    }
+    */
+   //TRAVERSING THE DOM:moving up and down.looking parent node,child node and siblings
+   let itemList= document.querySelector('#items');
+   //parentNode
+   /*
+   console.log(itemList.parentNode);//returns the parentNode of current element
+   itemList.parentNode.style.background='#ccc';
+   console.log(itemList.parentNode.parentNode.parentNode);// we can find granparents as well
+    */
+   //ParentElement// : same as parentNode
+   /*
+   console.log(itemList.parentElement);//returns the parentNode of current element
+   itemList.parentElement.style.background='#ccc';
+   console.log(itemList.parentElement.parentElement.parentElement);// we can find granparents as well w/c in this case gives body tag
+    */
+   //childNode
+   /*
+   /console.log(itemList.childNodes);//returns NodeList/array of child elements
+        // NB, in this collection white space/line brake take a place as text element, so becareful when using it
+    */
+        //children: solves the above problem in childNodes
+    /*
+    console.log(itemList.children);
+    console.log(itemList.children[1]);
+    itemList.children[1].style.background='yellow';
+    */
+   //firstChild property
+   /*
+   console.log(itemList.firstChild); //returns text for white space as firstchild if there is space before first element
+   */
+   //firstElementChild: solves firstChild prolem
+   /*
+   console.log(itemList.firstElementChild);
+   itemList.firstElementChild.textContent='content added with firstElementChild proeprty';
+    */
+   //lastChild property
+   /*
+   console.log(itemList.firstChild); //returns text for white space as lastchild if there is space after last element
+   */
+    //lastElementChild: like firstElementChild this one solves the problem of lastchild which grabs white spaces
+    /*
+    console.log(itemList.lastElementChild);
+    itemList.lastElementChild.textContent='content added with lastElementChild proeprty';
+    */
+   //nextSiblings
+        // console.log(itemList.nextSibling);//gives text node for whitespace if there is space
+    //nextElementSiblings
+        //console.log(itemList.nextElementSibling);
+    //previousSiblings
+        //console.log(itemList.previousSibling);//gives text node for whitespace if there is space
+   //priviousElementSiblings
+        /*console.log(itemList.previousElementSibling);
+         itemList.previousElementSibling.style.color='green';
+        */
+    //createElement
+    let newDiv=document.createElement('div')   //creates div
+
+
+   
